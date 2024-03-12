8c4dcc454a5931020afae74852c73b82885c8e9c
ingekn <133350400+ingekn@users.noreply.github.com>
2024-03-12 20:32:42 +0000

    Installed two new components for 1 assignment

--- my-first-project/src/app/app.component.ts
+++ my-first-project/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  name = 'My first app';
+  title = 'My first app';
 }
--- my-first-project/src/app/app.module.ts
+++ my-first-project/src/app/app.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
