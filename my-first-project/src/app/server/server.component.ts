5b68d3555af1f4fd349ab3fe4baf5126dbc67ef2
ingekn <133350400+ingekn@users.noreply.github.com>
2024-03-13 20:19:26 +0000

    Property binding/ Event binding

--- my-first-project/src/app/server/server.component.ts
+++ my-first-project/src/app/server/server.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
 })
-export class ServerComponent {}
+export class ServerComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created! ';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+  ngOnInit() {}
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+}
